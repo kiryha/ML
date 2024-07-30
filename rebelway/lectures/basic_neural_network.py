7d2ba3b72ae85ee28d3c7fb23995166a6af19ed3
KRYSKO Kiryha <KKO8@dsone.3ds.com>
2024-07-30 12:45:24 -0400

    neurons

--- rebelway/lectures/basic_neural_network.py
+++ rebelway/lectures/basic_neural_network.py
@@ -1,1 +1,57 @@
-import numpy
+import numpy as np
+
+
+def loss_function(w, b, train_data):
+
+    result = 0
+    count = len(train_data)
+
+    for i in range(count):
+        x = train_data[i][0]
+        y = x*w + b
+        d = y - train_data[i][1]
+
+        result += d*d
+
+    mean = result/count
+
+    return mean
+
+
+def train(train_data):
+    
+    np.random.seed(0)
+    w = np.random.uniform(0, 10, 1)  # Weight
+    b = np.random.uniform(0, 5, 1)  # Bias
+    epsilon = 0.001
+    epochs = 200
+    learning_rate = 0.01
+
+    print('LOSS: ', loss_function(w, b, train_data))
+
+    # Forward pass
+    for i in range(epochs):
+        c = loss_function(w, b, train_data)
+        cost_distance = (loss_function(w+epsilon, b, train_data) - c) / epsilon  # Simplified Derivative formula
+        bias_distance = (loss_function(w, b+epsilon, train_data) - c) / epsilon
+
+        w -= cost_distance * learning_rate
+        b -= bias_distance * learning_rate
+
+        if i % 10 == 0:
+            print(loss_function(w, b, train_data))
+
+    print('-'*20)
+    print('w: ', w)  # w = 1.9 >> train_data[1 column] = train_data[0 column] * 2 <-- (2 ~= 1.9)
+    print('b: ', b)
+
+
+if __name__ == "__main__":
+
+    train_data = [[0, 0],
+                  [1, 2],
+                  [2, 4],
+                  [4, 8],
+                  [8, 16]]
+
+    train(train_data)
---
+++ rebelway/lectures/two_features_neural_network.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+def loss_function(w1, w2, b, train_data):
+
+    result = 0
+    count = len(train_data)
+
+    for i in range(count):
+        x1 = train_data[i][0]
+        x2 = train_data[i][1]
+        y = sigmoid(x1 * w1 + x2 * w2 + b)
+        d = y - train_data[i][2]
+        result += d * d
+
+    mean = result / count
+
+    return mean
+
+
+def sigmoid(x):
+
+    return 1/(1 + np.exp(-x))
+
+
+def train(train_data):
+
+    # np.random.seed(0)
+    w1 = np.random.uniform(0, 1, 1)  # Weight
+    w2 = np.random.uniform(0, 1, 1)  # Weight
+    b = np.random.uniform(0, 1, 1)
+
+    epochs = 5000
+    epsilon = 0.01
+    learning_rate = 0.1
+
+    print('Init Loss: ', loss_function(w1, w2, b, train_data))
+
+    for i in range(epochs):
+        c = loss_function(w1, w2, b, train_data)
+        # print(f'w1: {w1} w2: {w2} loss: {c}')
+
+        dw1 = (loss_function(w1 + epsilon, w2, b, train_data) - c) / epsilon
+        dw2 = (loss_function(w1, w2 + epsilon, b, train_data) - c) / epsilon
+        bd = (loss_function(w1, w2, b + epsilon, train_data) - c) / epsilon
+
+        w1 -= learning_rate * dw1
+        w2 -= learning_rate * dw2
+        b -= learning_rate * bd
+
+    print('---------------')
+    print(f'w1: {w1} w2: {w2}  b: {b} loss: {loss_function(w1, w2, b, train_data)}')
+    print('---------------')
+
+    # Test predictions
+    for i in range(2):
+        for j in range(2):
+            print(f'i {i} | j {sigmoid(i*w1 + j*w2 + b)} ')
+
+
+if __name__ == "__main__":
+
+    # Train Data row = [feature 1, feature 2, label (prediction)]
+
+    train_data = [[0, 0, 0],
+                  [1, 0, 1],
+                  [0, 1, 1],
+                  [1, 1, 1]]
+
+    train(train_data)
